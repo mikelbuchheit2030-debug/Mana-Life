3ff66409eae5781ca1fe3404791e88cf743d9c89
mikelbuchheit2030-debug <mikel.buchheit.2030@gfps.k12.mt.us>
2025-12-05 04:10:33 -0700

    Add crafting table functionality and cave exploration

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 //Variables
 let wood = 0;
 let stone = 0;
+let craftingTable = false;
+let caveFound = false;
 
-//Resource collection
+//Basic functions
 window.collectStick = function() {
   wood ++;
 }
 
 window.collectStone = function() {
   stone ++
+}
+
+window.makeCraftingTable = function(){
+  if (wood >= 3 && stone >= 2) {
+    craftingTable = true;
+    wood -= 3;
+    stone -= 2;
+    document.getElementById("inventoryDisplay").innerHTML =
+      "You have<br>" + wood + " sticks<br>" + stone + " stones";
+  }
 }
 
 //Tab system
@@ -23,20 +35,51 @@ window.tabToForest = function() {
   document.getElementById("forest").style.display = "block";
   document.getElementById("explore").style.display = "none";
   document.getElementById("inventory").style.display = "none";
-  
+  document.getElementById("cave").style.display = "none";
+  if (caveFound === true) {
+    document.getElementById("caveTab").style.display = "block";
+  }
 }
 
 window.tabToExplore = function() {
   document.getElementById("forest").style.display = "none";
   document.getElementById("explore").style.display = "block";
   document.getElementById("inventory").style.display = "none";
-
+    document.getElementById("cave").style.display = "none";
+  if (caveFound === true) {
+    document.getElementById("caveTab").style.display = "block";
+  }
+  
 }
 
 window.tabToInventory = function() {
   document.getElementById("forest").style.display = "none";
   document.getElementById("explore").style.display = "none";
   document.getElementById("inventory").style.display = "block";
-  document.getElementById("inventoryDisplay").innerHTML= "You have<br>" + wood + " sticks<br>" + stone + "stones"
+    document.getElementById("cave").style.display = "none";
+  document.getElementById("inventoryDisplay").innerHTML= "You have<br>" + wood + " sticks<br>" + stone + " stones"
+  if (wood >= 3 && stone >= 2) {
+    document.getElementById("makeCraftingTable").style.display = "block"
+  }
+  if (caveFound === true) {
+    document.getElementById("caveTab").style.display = "block";
+  }
+}
 
+window.tabToCave = function() {
+  document.getElementById("forest").style.display = "none";
+  document.getElementById("explore").style.display = "none";
+  document.getElementById("inventory").style.display = "none";
+  document.getElementById("cave").style.display = "block";
+}
+
+//Exploration
+window.explore = function(){
+  if (craftingTable === true) {
+    caveFound = true;
+    console.log(caveFound)
+  }
+  if (caveFound === true) {
+    document.getElementById("caveTab").style.display = "block";
+  }
 }
